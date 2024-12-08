41317b3859bf93e8aebba6a38f7239045deccec3
modamo-gh <morganthemosaic@gmail.com>
2024-12-08 11:03:45 -0500

    Day 8 Part 1

---
+++ 08/1.ts
@@ -0,0 +1,50 @@
+import { readFileSync } from "fs";
+
+const map = readFileSync("input.txt", "utf8")
+	.trim()
+	.split(/\n/)
+	.map((line) => line.trim().split(""));
+
+const antennas = [];
+
+for (let row = 0; row < map.length; row++) {
+	for (let column = 0; column < map[row].length; column++) {
+		if (map[row][column] !== ".") {
+			antennas.push({ row, column, frequency: map[row][column] });
+		}
+	}
+}
+
+const uniqueAntinodeLocations = new Set<string>();
+
+for (let i = 0; i < antennas.length; i++) {
+	for (let j = 0; j < antennas.length; j++) {
+		if (
+			(antennas[i].column !== antennas[j].column ||
+			antennas[i].row !== antennas[j].row) &&
+			antennas[i].frequency === antennas[j].frequency
+		) {
+			const dc = antennas[j].column - antennas[i].column;
+			const dr = antennas[j].row - antennas[i].row;
+
+			const potentialAntinodeColumn = antennas[j].column + dc;
+			const potentialAntinodeRow = antennas[j].row + dr;
+
+			if (
+				potentialAntinodeRow >= 0 &&
+				potentialAntinodeRow < map.length &&
+				potentialAntinodeColumn >= 0 &&
+				potentialAntinodeColumn < map[potentialAntinodeRow].length
+			) {
+				uniqueAntinodeLocations.add(
+					JSON.stringify({
+						potentialAntinodeRow,
+						potentialAntinodeColumn
+					})
+				);
+			}
+		}
+	}
+}
+
+console.log(uniqueAntinodeLocations.size);
